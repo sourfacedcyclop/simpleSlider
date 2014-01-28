434e9c9bdabd9eef1b1254942d5f74ffe51e3235
Thomas Scheuneman <thomas@tswebvisions.com>
2014-01-27 23:36:57 -0700

    Update File
    Fixed full code not showing up

--- jquery.simpleSlider.js
+++ jquery.simpleSlider.js
@@ -1,85 +1,198 @@
-(function(b) {
-  b.fn.simpleSlider = function(h) {
-    var c = b.extend({fadeTime:500, autoplay:!0, container:this, navContainer:null, wantNav:!0, next:"#simpleSliderNext", previous:"#simpleSliderPrevious", interval:1E4, pauseOnHover:!0, keyboardNav:!0}, h);
-    return this.each(function() {
-      function k() {
-        f.end();
-        b(e[a]).stop(!1, !0).fadeOut(c.fadeTime);
-        a++;
-        a >= g && (a = 0);
-        b(e[a]).stop(!1, !0).fadeIn(c.fadeTime);
-        c.wantNav && (0 == a && b(d[g - 1]).removeClass("active"), b(d[a - 1]).removeClass("active"), b(d[a]).addClass("active"));
-        f.next()
-      }
-      function m() {
-        f.end();
-        b(e[a]).stop(!1, !0).fadeOut(c.fadeTime);
-        a--;
-         -1 >= a && (a = g - 1);
-        b(e[a]).stop(!1, !0).fadeIn(c.fadeTime);
-        f.next();
-        c.wantNav && (a == g - 1 && b(d[0]).removeClass("active"), b(d[a + 1]).removeClass("active"), b(d[a]).addClass("active"))
-      }
-      function h() {
-        for(var a = "", b = 0;b < g;b++) {
-          a += '<div class="simpleSliderNavItem"></div> '
+/*
+Author : Thomas Scheuneman
+Name : simpleSLider
+Version : 1.0
+Author Site : http://tswebvisions.com
+*/
+(function( $ ){
+"use strict";
+    $.fn.simpleSlider = function(options) {
+	//Our defalut options
+    var defaults = {
+		fadeTime: 500,
+		autoplay: true,
+		container: this,
+		navContainer: null,
+		wantNav: true,
+		next: "#simpleSliderNext",
+		previous: "#simpleSliderPrevious",
+		interval: 10000,
+		pauseOnHover: true,
+		keyboardNav: true
+	};
+	var settings = $.extend(defaults, options); 
+	return this.each(function() {
+		//Set our variables for slides, our timeout, amount, and our counter
+        var slides, amount, counter;
+		
+		/*
+		Function for running our animation forward, this is engaed on the next slide button, 
+		and on the natural continuation of all other alider actions
+		*/
+        function fadeForward() {
+			//Clear any timeout
+			timeOut.end()
+			//fadeout current slide
+            $(slides[counter]).stop(false, true).fadeOut(settings.fadeTime);
+			//add to counter
+            counter++;
+			//if counter has reached end, start over
+            if (counter >= amount) {
+				counter = 0;
+			}
+			//fadein new slide
+            $(slides[counter]).stop(false, true).fadeIn(settings.fadeTime);
+			//update progess bar and continue loop
+			//If we have gone fullcircle, make the right element is removed if the user wants to use the nav
+			if(settings.wantNav) {
+				if(counter == 0) {
+					$(sliderNavButtons[amount - 1]).removeClass("active");		
+				}
+				$(sliderNavButtons[counter - 1]).removeClass("active");
+				$(sliderNavButtons[counter]).addClass("active");
+			}
+			timeOut.next();
         }
-        jQuery(c.navContainer).append('<div id="simpleSliderNav">' + a + "</div>")
-      }
-      var e, g, a;
-      if(c.pauseOnHover) {
-        jQuery(c.container).children().on({mouseenter:function() {
-          f.pause()
-        }, mouseleave:function() {
-          f.resume()
-        }})
-      }
-      c.keyboardNav && jQuery(document.documentElement).keydown(function(a) {
-        37 == a.keyCode && m();
-        39 == a.keyCode && k()
-      });
-      jQuery(c.next).click(function() {
-        k()
-      });
-      jQuery(c.previous).click(function() {
-        m()
-      });
-      jQuery(document).on("click", ".simpleSliderNavItem", function() {
-        var l = b(this).index();
-        f.end();
-        c.wantNav && (b(d[a]).removeClass("active"), b(d[l]).addClass("active"));
-        b(e[a]).stop(!1, !0).fadeOut(c.fadeTime);
-        a = l;
-        -1 >= a && (a = g - 1);
-        b(e[l]).stop(!1, !0).fadeIn(c.fadeTime);
-        f.next();
-        c.wantNav && a == g - 1 && b(d[0]).removeClass("active")
-      });
-      e = b(c.container).children();
-      g = e.length;
-      a = 0;
-      if(c.wantNav) {
-        h();
-        var d = b("#simpleSliderNav").children();
-        b(d[a]).addClass("active")
-      }
-      var f = new function(a, b) {
-        var d, f, e = b;
-        this.pause = function() {
-          window.clearTimeout(d);
-          e -= new Date - f
-        };
-        this.resume = function() {
-          c.autoplay && (f = new Date, d = window.setTimeout(a, e))
-        };
-        this.end = function() {
-          window.clearTimeout(d)
-        };
-        this.next = function() {
-          c.autoplay && (d = window.setTimeout(a, b), e = b, f = new Date)
-        };
-        this.resume()
-      }(k, c.interval)
-    })
-  }
+		//Function for previous button click.
+        function fadeBack() {
+			timeOut.end();
+			//fadeout current slide
+            $(slides[counter]).stop(false, true).fadeOut(settings.fadeTime);
+			//subtract from counter
+            counter--;
+			//if counter has reached end, start over
+            if (counter <= -1){
+				counter = amount - 1;
+			}
+			//update progess bar and continue loop
+            $(slides[counter]).stop(false, true).fadeIn(settings.fadeTime);
+            //update progess bar and continue loop
+			timeOut.next();
+			//If we have gone fullcircle, make the right element is removed if the user wants to use the nav
+			if(settings.wantNav) {
+				if(counter == amount - 1) {
+					$(sliderNavButtons[0]).removeClass("active");		
+				}
+				$(sliderNavButtons[counter + 1]).removeClass("active");
+				$(sliderNavButtons[counter]).addClass("active");
+			}
+        }
+		//Function for a timer
+		function Timer(callback, delay) {
+			//Create variables
+			var timer, start, remainingTime = delay;
+			this.pause = function() {
+				window.clearTimeout(timer);
+				remainingTime -= new Date() - start;
+			};
+			this.resume = function() {
+			if(settings.autoplay) {
+					start = new Date();
+					timer = window.setTimeout(callback, remainingTime);
+				}
+			};
+			this.end = function() {
+				window.clearTimeout(timer)
+			};
+			this.next = function() {
+				if(settings.autoplay) {
+					timer = window.setTimeout(callback, delay);
+					remainingTime = delay;
+					start = new Date();
+				}
+			};
+			this.resume();
+		}
+		//If the user wants sliders to pause on their hover
+		if(settings.pauseOnHover) {
+			jQuery(settings.container).children().on(
+			{
+				mouseenter: function() {
+					timeOut.pause();
+				},
+				mouseleave: function() {
+					timeOut.resume();
+				}
+			});
+		}
+		//For Keyinput
+		if(settings.keyboardNav) {
+			jQuery(document.documentElement).keydown(function(event) {
+				if(event.keyCode == 37) {
+					fadeBack();
+				}
+				if(event.keyCode == 39) {
+					fadeForward();
+				}
+			});
+		}
+		//On next button click, go forward
+		jQuery(settings.next).click(function() {
+			fadeForward();
+		});
+
+		//On previouys button click, for backeards
+		jQuery(settings.previous).click(function() {
+			fadeBack();
+		});
+		//On the nav button click
+		jQuery(document).on('click', '.simpleSliderNavItem', function() {
+		//Find which button has been clicked
+			var id = $(this).index();
+			//Reset timer
+			timeOut.end();
+			//Remove previous slide highlight and add for current slide if the user wants to use the nav
+			if(settings.wantNav) {
+				$(sliderNavButtons[counter]).removeClass("active");
+				$(sliderNavButtons[id]).addClass("active");
+			}
+			//fadeout current slide
+            $(slides[counter]).stop(false, true).fadeOut(settings.fadeTime);
+			//the counter now equals the current slide
+            counter = id;
+			//if counter has reached end, start over
+            if (counter <= -1){
+				counter = amount - 1;
+			}
+			//update progess bar and continue loop
+            $(slides[id]).stop(false, true).fadeIn(settings.fadeTime);
+
+            //update progess bar and continue loop
+			timeOut.next();
+						//If we have gone fullcircle, make the right element is removed if the user wants to use the nav
+			if(settings.wantNav) {
+				if(counter == amount - 1) {
+					$(sliderNavButtons[0]).removeClass("active");		
+				}
+			}
+		});
+		//Function to create our navigation
+		function circularNav() {
+			var itemNav = '';
+			var nav = '';
+			for (var x = 0; x < amount; x++) {
+				itemNav += '<div class="simpleSliderNavItem"></div> ';
+			}
+			jQuery(settings.navContainer).append('<div id="simpleSliderNav">' + itemNav + '</div>');
+		}
+		//Get slides
+        slides = $(settings.container).children();
+		//Get the number of slides and the counter counter
+        amount = slides.length;
+        counter=0;
+		//If the user wants to add the navigation
+		if(settings.wantNav) {
+			//Create the naviagtion
+			circularNav();
+			//Get the navigation info
+			var sliderNavButtons = $('#simpleSliderNav').children();
+			//Add active class to current slide
+			$(sliderNavButtons[counter]).addClass("active");
+		}
+        // Set our timeout to move along the content
+
+			var timeOut = new Timer(fadeForward, settings.interval);
+		});
+    };
 })(jQuery);
+// <x\\>
